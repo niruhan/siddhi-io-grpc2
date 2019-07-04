de9862c45f50123704e08c2a9304d0b51cd7a1e6
niruhan <niruhanv@gmail.com>
2019-07-04 10:42:16 +0530

    first commit

---
+++ component/src/test/java/org/wso2/extension/siddhi/io/grpc/sink/TestCaseOfGrpcSink.java
@@ -0,0 +1,6 @@
+package org.wso2.extension.siddhi.io.grpc.sink;
+
+public class TestCaseOfGrpcSink {
+        // If you will know about this related testcase,
+        //refer https://github.com/wso2-extensions/siddhi-io-file/blob/master/component/src/test
+}
---
+++ component/src/test/java/org/wso2/extension/siddhi/io/grpc/source/TestCaseOfGrpcSource.java
@@ -0,0 +1,6 @@
+package org.wso2.extension.siddhi.io.grpc.source;
+
+public class TestCaseOfGrpcSource {
+        // If you will know about this related testcase,
+        //refer https://github.com/wso2-extensions/siddhi-io-file/blob/master/component/src/test
+}

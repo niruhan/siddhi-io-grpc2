0f3147af3c3134b52dcf78a4bf727104221a2905
niruhan <niruhanv@gmail.com>
2019-07-08 17:26:33 +0530

    add proto file for SI MI connect and basinc sink init implementation

--- component/src/main/java/io/siddhi/extension/io/grpc/sink/GRPCSink.java
+++ component/src/main/java/io/siddhi/extension/io/grpc/sink/GRPCSink.java
@@ -17,10 +17,13 @@
  */
 package io.siddhi.extension.io.grpc.sink;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.siddhi.annotation.Example;
 import io.siddhi.annotation.Extension;
 import io.siddhi.core.config.SiddhiAppContext;
 import io.siddhi.core.exception.ConnectionUnavailableException;
+import io.siddhi.core.exception.SiddhiAppRuntimeException;
 import io.siddhi.core.stream.ServiceDeploymentInfo;
 import io.siddhi.core.stream.output.sink.Sink;
 import io.siddhi.core.util.config.ConfigReader;
@@ -28,8 +31,15 @@ import io.siddhi.core.util.snapshot.state.State;
 import io.siddhi.core.util.snapshot.state.StateFactory;
 import io.siddhi.core.util.transport.DynamicOptions;
 import io.siddhi.core.util.transport.OptionHolder;
+import io.siddhi.extension.io.grpc.InvokeSequenceGrpc;
+import io.siddhi.extension.io.grpc.InvokeSequenceGrpc.InvokeSequenceBlockingStub;
+import io.siddhi.extension.io.grpc.InvokeSequenceGrpc.InvokeSequenceStub;
 import io.siddhi.query.api.definition.StreamDefinition;
 import org.apache.log4j.Logger;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This is a sample class-level comment, explaining what the extension class does.
@@ -59,12 +69,11 @@ import org.apache.log4j.Logger;
 public class GRPCSink extends Sink {
     private static final Logger logger = Logger.getLogger(GRPCSink.class.getName());
 
-//    private final ManagedChannel channel;
-//    private final RouteGuideBlockingStub blockingStub;
-//    private final RouteGuideStub asyncStub;
-//
-//    private Random random = new Random();
-//    private TestHelper testHelper;
+    private ManagedChannel channel; //todo: make final
+    private InvokeSequenceBlockingStub blockingStub;
+    private InvokeSequenceStub asyncStub;
+    private SiddhiAppContext siddhiAppContext;
+    private Random random = new Random();
 
     /**
      * Returns the list of classes which this sink can consume.
@@ -106,16 +115,25 @@ public class GRPCSink extends Sink {
     @Override
     protected StateFactory init(StreamDefinition streamDefinition, OptionHolder optionHolder, ConfigReader configReader,
                                 SiddhiAppContext siddhiAppContext) {
-
-//        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
-//        blockingStub = RouteGuideGrpc.newBlockingStub(channel);
-//        asyncStub = RouteGuideGrpc.newStub(channel);
+        this.siddhiAppContext = siddhiAppContext;
+        String host = ""; //todo: get from somewhere
+        int port = 0;
+        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        blockingStub = InvokeSequenceGrpc.newBlockingStub(channel);
+        asyncStub = InvokeSequenceGrpc.newStub(channel);
         return null;
     }
 
     @Override
-    public void publish(Object o, DynamicOptions dynamicOptions, State state) throws ConnectionUnavailableException {
+    public void publish(Object payload, DynamicOptions dynamicOptions, State state) throws ConnectionUnavailableException {
+        String payloadAsJSON = generateJSONStringFromObjectPayload(payload);
+        String sequenceName = "";
+        Map<String, String>
 
+    }
+
+    private String generateJSONStringFromObjectPayload(Object payload) {
+        return null; //todo: implement function
     }
 
     /**
@@ -145,5 +163,14 @@ public class GRPCSink extends Sink {
     @Override
     public void destroy() {
 
+    }
+
+    @Override
+    public void shutdown() {
+        try {
+            channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new SiddhiAppRuntimeException(siddhiAppContext.getName() + ": " + e.getMessage());
+        }
     }
 }

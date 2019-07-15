53849f28906ef6881cb19a9995f769268cc8531f
niruhan <niruhanv@gmail.com>
2019-07-15 10:17:13 +0530

    add client request functions

--- component/src/main/java/io/siddhi/extension/io/grpc/sink/GRPCSink.java
+++ component/src/main/java/io/siddhi/extension/io/grpc/sink/GRPCSink.java
@@ -17,8 +17,12 @@
  */
 package io.siddhi.extension.io.grpc.sink;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import io.grpc.*;
+import io.grpc.stub.ClientCalls;
 import io.siddhi.annotation.Example;
 import io.siddhi.annotation.Extension;
 import io.siddhi.core.config.SiddhiAppContext;
@@ -33,12 +37,18 @@ import io.siddhi.core.util.transport.DynamicOptions;
 import io.siddhi.core.util.transport.OptionHolder;
 import io.siddhi.extension.io.grpc.util.GRPCStubHolder;
 import io.siddhi.extension.io.grpc.util.GrpcBlockingStub;
+import io.siddhi.extension.io.grpc.util.GRPCService.Request;
+import io.siddhi.extension.io.grpc.util.GRPCService.EmptyResponse;
 import io.siddhi.extension.io.grpc.util.Message;
 import io.siddhi.query.api.definition.StreamDefinition;
 import org.apache.log4j.Logger;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * This is a sample class-level comment, explaining what the extension class does.
@@ -67,12 +77,12 @@ import java.util.concurrent.TimeUnit;
 
 public class GRPCSink extends Sink {
     private static final Logger logger = Logger.getLogger(GRPCSink.class.getName());
-
-    private ManagedChannel channel; //todo: make final
     private GrpcBlockingStub blockingStub;
 //    private InvokeSequenceStub asyncStub;
     private SiddhiAppContext siddhiAppContext;
     private Random random = new Random();
+    private Channel channel;
+    private String serviceName;
 
     /**
      * Returns the list of classes which this sink can consume.
@@ -115,28 +125,61 @@ public class GRPCSink extends Sink {
     protected StateFactory init(StreamDefinition streamDefinition, OptionHolder optionHolder, ConfigReader configReader,
                                 SiddhiAppContext siddhiAppContext) {
         this.siddhiAppContext = siddhiAppContext;
-        String host = ""; //todo: get from somewhere
-        int port = 0;
-        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        this.serviceName = ""; //todo: get from options holder
+
+        channel = ManagedChannelBuilder.forTarget("dns:///localhost:" + 8000)
+                .usePlaintext(true)
+                .build();
+
         blockingStub = new GrpcBlockingStub(channel);
         GRPCStubHolder.getInstance().setBlockingStub(blockingStub);
-//        GRPCStubHolder.getInstance().setAsyncStub(asyncStub);
+
         return null;
     }
 
     @Override
     public void publish(Object payload, DynamicOptions dynamicOptions, State state) throws ConnectionUnavailableException {
-//        String payloadAsJSON = generateJSONStringFromObjectPayload(payload);
-//        String sequenceName = "";
-//        SequenceCallRequest.Builder requestBuilder = SequenceCallRequest.newBuilder();
-////        Map<String, String> synapseConfigs = requestBuilder.getSynapseConfigsMap();
-//        requestBuilder.putSynapseConfigs("", "");
-//        requestBuilder.setPayloadAsJSON(payloadAsJSON);
-//        requestBuilder.setSequenceName(sequenceName);
-//        SequenceCallRequest sequenceCallRequest = requestBuilder.build();
-//
-//        SequenceCallResponse response = blockingStub.callSequenceWithResponse(sequenceCallRequest); //todo: have to pass this request smhow to grpc source
-        Message response = blockingStub.executeUnary();
+        ClientCall<Request, EmptyResponse> call = channel.newCall(CREATE_METHOD, CallOptions.DEFAULT);
+        Request request = new Request();
+        ListenableFuture<EmptyResponse> res = ClientCalls.futureUnaryCall(call, request);
+        res.addListener(() ->  { }, MoreExecutors.directExecutor());
+
+        Futures.addCallback(res, new FutureCallback<EmptyResponse>() {
+            @Override
+            public void onSuccess(EmptyResponse result) {
+                System.out.println("Success!");
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println("Failure");
+            }
+        });
+    }
+
+    MethodDescriptor<Request, EmptyResponse> CREATE_METHOD =
+            MethodDescriptor.newBuilder(
+                    marshallerFor(Request.class),
+                    marshallerFor(EmptyResponse.class))
+                    .setFullMethodName(
+                            MethodDescriptor.generateFullMethodName(serviceName, "Create"))
+                    .setType(MethodDescriptor.MethodType.UNARY)
+                    .setSampledToLocalTracing(true)
+                    .build();
+
+    <T> MethodDescriptor.Marshaller<T> marshallerFor(Class<T> clz) {
+        return new MethodDescriptor.Marshaller<T>() {
+            @Override
+            public InputStream stream(T value) {
+                return new ByteArrayInputStream((byte[]) value);
+            }
+
+            @Override
+            public T parse(InputStream stream) {
+                return null; //gson.fromJson(new InputStreamReader(stream, StandardCharsets.UTF_8), clz);
+                //todo: find way to get byte[] from inputstream
+            }
+        };
     }
 
     private String generateJSONStringFromObjectPayload(Object payload) {
---
+++ component/src/main/java/io/siddhi/extension/io/grpc/util/BinaryMessage.java
@@ -0,0 +1,33 @@
+package io.siddhi.extension.io.grpc.util;
+
+import com.google.protobuf.GeneratedMessageV3;
+import com.google.protobuf.Message;
+
+public class BinaryMessage extends GeneratedMessageV3 {
+    byte[] value;
+
+    @Override
+    protected FieldAccessorTable internalGetFieldAccessorTable() {
+        return null;
+    }
+
+    @Override
+    protected Message.Builder newBuilderForType(BuilderParent parent) {
+        return null;
+    }
+
+    @Override
+    public Message.Builder newBuilderForType() {
+        return null;
+    }
+
+    @Override
+    public Message.Builder toBuilder() {
+        return null;
+    }
+
+    @Override
+    public Message getDefaultInstanceForType() {
+        return null;
+    }
+}
